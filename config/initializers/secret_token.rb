4b3286bd2787bd7dfa5c113ed9864a599bdf417e
Joe Lee <joelee@stanford.edu>
2014-05-22 17:49:40 -0700

    create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sleep::Application.config.secret_key_base = '5cc0bfafa6253d03cea1cf34edc7baac98658f03e24191e7d988332c87fcd490708c43787879a2b1706b5e7c850275b50bf7ac9333145cd5b9721061e9666e34'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sleep::Application.config.session_store :cookie_store, key: '_sleep_session'
